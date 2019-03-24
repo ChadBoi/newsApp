8fcbe22bd86cdfd3ba7203bb59b6aac086baa731
Chad Baxter <chaddudeguy@gmail.com>
2019-03-24 01:33:39 -0600

    final bugfixing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 export class AppComponent {
   title = 'newsApp';
 
-  constructor(private auth: AuthService, private router: Router) {
+  constructor(public auth: AuthService, private router: Router) {
 
   }
 
